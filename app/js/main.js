9179fe6762d3b046a8c53f351023c4af6be75654
vitaliybalayan <vitaliy.balayan@gmail.com>
2020-03-25 20:16:15 +0500

    firstblock

--- app/js/main.js
+++ app/js/main.js
@@ -1,24 +1,16 @@
-$(document).mousemove(function(event){
+// $('.main-slider--item').mousemove(function(event){
 
-	var x = (event.clientX/$(window).width())-0.5;
-	var y = (event.clientY/$(window).height())-0.5;
+// 	var x = (event.clientX/$(window).width())-0.5;
+// 	var y = (event.clientY/$(window).height())-0.5;
 
-	TweenLite.to($('.perspective-image, .slide-text-animate'), 0.6, {
-		rotationY: 5*x,
-		rotationX: 5*y,
-		ease: Power1.easeOut,
-		transformPerspective: 400,
-		transformOrigin: "center"
-	});
+// 	TweenLite.to($('.perspective-image'), 0.6, {
+// 		rotationY: 5*x,
+// 		rotationX: 5*y,
+// 		ease: Power1.easeOut,
+// 		transformPerspective: 400,
+// 		transformOrigin: "center"
+// 	});
 
-	// console.log(x);
-	// $('.main-slider--item--title_text').css('background-position',event.pageX+'px '+event.pageY+'px');
-});
-
-$('.main-slider--item--title_text').ready(function() {
-	console.log('Картинка загружена');
-});
-
-$(document).ready(function() {
-	console.log('Страница загружена');
-});
+// 	// console.log(x);
+// 	$('.main-slider--item--title_text').css('background-position',event.pageX/7+'px '+event.pageY/7+'px');
+// });
