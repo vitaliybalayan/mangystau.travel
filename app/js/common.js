af3d4a0ce70adbc4aec6a3fc520edb681435dedb
vitaliybalayan <vitaliy.balayan@gmail.com>
2020-03-28 17:34:54 +0500

    inner-page v1

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,9 @@
 $(function() {
-	// 
+	
+	$('.inner-page--main_block--background').hover(function() {
+		$('.i-p--m_b--f-link').addClass('hovered');
+	}, function() {
+		$('.i-p--m_b--f-link').removeClass('hovered');
+	});
+
 });
