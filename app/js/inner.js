05ec1d02e1f11819b5cfc9e77af6b257b891e695
vitaliybalayan <vitaliy.balayan@gmail.com>
2020-03-30 03:44:49 +0500

    photogallery v1

--- app/js/inner.js
+++ app/js/inner.js
@@ -1,16 +1,8 @@
-$(document).on('click', '[data-aside_show="fullpage"]', function(block) {
+$(document).on('click', '.inner-page--menu--sticky [data-aside_show="fullpage"]', function(block) {
 
-	var scrollHeight = $('.inner-page--main_block--background').height() / 2;
+	var aside_id = block.currentTarget.dataset.aside_id;
 
-	$('html, body').animate({
-		scrollTop: scrollHeight
-	}, 500, function(event) {
-		$('.inner-page--menu--sticky').addClass('no--sticky');
-		
-		var aside_id = block.currentTarget.dataset.aside_id;
-		AsideShowFullPage(aside_id);
-		
-	});
+	AsideShowFullPage(aside_id);
 
 	return false;
 });
@@ -18,45 +10,169 @@ $(document).on('click', '[data-aside_show="fullpage"]', function(block) {
 function AsideShowFullPage(id) {
 	// console.log('Время магии');
 
-	var inner_background = $('.inner-page--main_block--background');
+	var scrollHeight = $('.inner-page--main_block--background').height() / 2;
+
+	var asideButton = $('.aside-content-button[data-aside_block='+ id +']');
+
+	var asideButtonOldText = asideButton.html();
+	var asideButtonNewText = asideButton.data('next_text');
+
+	asideButton.removeClass('hidden');
+	asideButton.addClass('visible');
+	
+	asideButton.attr('data-next_text', asideButtonOldText);
+
+	$('html, body').animate({
+		scrollTop: scrollHeight
+	}, 500, function(event) {
+		asideButton.html(asideButtonNewText);
+		
+		$('.inner-page--menu--sticky').addClass('no--sticky');
+		
+		var inner_background = $('.inner-page--main_block--background');
+		var inner_content = $('.inner-page--content');
+		var inner_container = $('.inner-page--container');
+
+		var block = $('.inner-aside-block[data-aside_block='+ id +']');
+
+		var dustanceContainer = inner_container.outerWidth() - inner_content.outerWidth() - block.outerWidth();
+
+		var contentTop = block.offset().top - inner_background.offset().top - inner_background.outerHeight() - 50;
+		var contentLeft = inner_content.outerWidth() + dustanceContainer;
+		
+
+		block.css({
+			'transform': 'translate(-'+ contentLeft +'px, -'+ contentTop +'px)',
+			'-webkit-transform': 'translate(-'+ contentLeft +'px, -'+ contentTop +'px)',
+			'-ms-transform': 'translate(-'+ contentLeft +'px, -'+ contentTop +'px)',
+		});
+		
+
+		setTimeout(function() {
+			block.css({
+				'position': 'absolute',
+				'min-width': ''+ inner_container.outerWidth() +'px',
+			});
+			
+
+			// inner_container.fadeOut();
+			inner_container.animate({
+				opacity: '0',
+				userSelect: 'none'
+			}, 100);
+
+		}, 600);
+
+
+
+
+		setTimeout(function() {
+			block.css({
+				'transform': 'unset',
+				'-webkit-transform': 'unset',
+				'-ms-transform': 'unset',
+			});
+
+			block.addClass('showing');
+
+			block.appendTo('.aside-content');
+		}, 700);
+	});
+
+}
+
+function AsideHideFullPage(id) {
+	var asideButton = $('.aside-content-button[data-aside_block='+ id +']');
+	// console.log(asideButton[0]);
+
+	var asideButtonOldText = asideButton.html();
+	var asideButtonNewText = asideButton[0].dataset.next_text;
+
 	var inner_content = $('.inner-page--content');
 	var inner_container = $('.inner-page--container');
+	var menu_sticky = $('.inner-page--menu--sticky');
+
+	console.log(asideButtonNewText);
+
+	asideButton.removeClass('visible');
+	asideButton.addClass('hidden');
+	
+	asideButton.attr('data-next_text', asideButtonOldText);
 
 	var block = $('.inner-aside-block[data-aside_block='+ id +']');
-
-	var dustanceContainer = inner_container.outerWidth() - inner_content.outerWidth() - block.outerWidth();
-
-	var contentTop = block.offset().top - inner_background.offset().top - inner_background.outerHeight() - 50;
-	var contentLeft = inner_content.outerWidth() + dustanceContainer;
-	
+	block.removeClass('showing');
 
 	block.css({
-		'transform': 'translate(-'+ contentLeft +'px, -'+ contentTop +'px)',
-		'-webkit-transform': 'translate(-'+ contentLeft +'px, -'+ contentTop +'px)',
-		'-ms-transform': 'translate(-'+ contentLeft +'px, -'+ contentTop +'px)',
+		'min-width': ''+ menu_sticky.outerWidth() +'px',
 	});
 
 	setTimeout(function() {
-		block.css({
-			'position': 'absolute',
-			'min-width': ''+ inner_container.outerWidth() +'px',
-			// 'left': '0',
-		});
+		inner_container.animate({
+			opacity: '1',
+			userSelect: 'unset'
+		}, 100);
+	}, 100);
 
-		inner_container.fadeOut();
-	}, 600);
+	var positionLeft = $('.inner-aside-grid').offset().left - $('.layout-space-left').outerWidth();
+	var positionTop = $('.inner-aside-grid').first().outerHeight();
 
+	// console.log($('.inner-aside-grid').offset());
 
 	setTimeout(function() {
 		block.css({
-			'transform': 'unset',
-			'-webkit-transform': 'unset',
-			'-ms-transform': 'unset',
+			'transform': 'translate('+ positionLeft +'px, '+ positionTop +'px)',
+			'-moz-transform': 'translate('+ positionLeft +'px, '+ positionTop +'px)',
+			'-ms-transform': 'translate('+ positionLeft +'px, '+ positionTop +'px)',
 		});
+	}, 300);
 
-		block.appendTo('.aside-content');
-	}, 700);
+	setTimeout(function() {
+	
+		menu_sticky.removeClass('no--sticky');
+		block.appendTo('.inner-aside-grid[data-aside_block='+ id +']');
+
+		block.css({
+			'transform': '',
+			'min-width': '',
+			'position': '',
+		});
+		
+	}, 600);
+
+	// setTimeout(function() {
+	// 	block.appendTo('.inner-aside-grid[data-aside_block='+ id +']');
+		
+	// 	block.css({
+	// 		'position': 'relative',
+	// 		'min-width': 'unset',
+	// 	});
+
+	// 	// inner_container.fadeOut();
+		// inner_container.animate({
+		// 	opacity: '1',
+		// 	userSelect: 'unset'
+		// }, 100);
+
+	// }, 600);
 
 
-
+	asideButton.html(asideButtonNewText);
 }
+
+$(document).on('click', '.aside-content-button', function(element) {
+
+	element.preventDefault();
+
+	if ($(this).hasClass('hidden')) {
+		AsideShowFullPage($(this).data('aside_block'));
+		
+		return false;
+	}
+
+	if ($(this).hasClass('visible')) {
+		AsideHideFullPage($(this).data('aside_block'));
+		
+		return false;
+	}
+
+});
